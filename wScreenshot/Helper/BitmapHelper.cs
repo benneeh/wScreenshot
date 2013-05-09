e10137cbc211934342ce5afb84629277b176d17f
benneh <b.bleher@gmx.net>
2013-05-09 19:02:43 +0200

    fhsdb

---
+++ wScreenshot/Helper/BitmapHelper.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace wScreenshot.Helper
+{
+    public static class BitmapHelper
+    {
+        public static void Save(Stream imageStream, BitmapMetadata metaData, Stream targetStream)
+        {
+            BitmapDecoder imgDecoder = BitmapDecoder.Create(imageStream, BitmapCreateOptions.None, BitmapCacheOption.Default);
+            BitmapFrame img = imgDecoder.Frames[0];
+            BmpBitmapEncoder bmpEnc = new BmpBitmapEncoder();
+            if (metaData != null)
+            {
+                bmpEnc.Metadata = metaData;
+            }
+            else
+            {
+                bmpEnc.Metadata = imgDecoder.Metadata;
+            }
+            bmpEnc.Frames.Add(img);
+            bmpEnc.Save(targetStream);
+
+            GifBitmapEncoder gifEnc;
+            JpegBitmapEncoder jpgEnc;
+            PngBitmapEncoder pngEnc;
+            TiffBitmapEncoder tiffEnc;
+            WmpBitmapEncoder wmpEnc;
+        }
+
+        public static BitmapImage Load(string path)
+        {
+            var bm = new BitmapImage(new Uri(path));
+            bm.Freeze();
+
+            BitmapDecoder imgDecoder = BitmapDecoder.Create(new Uri(path), BitmapCreateOptions.None, BitmapCacheOption.Default);
+            imgDecoder.
+            return null;
+        }
+    }
+}
